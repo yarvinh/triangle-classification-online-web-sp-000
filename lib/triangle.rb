df50429263e69668531018b7f03c5b1f9a158027
Yarvin Hernandez <yarhumher@hotmail.com>
2020-04-02 17:13:21 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,11 +6,13 @@ class Triangle
     @c = c
   end
   def kind
-    if @a == @b && @a == @c && @b == @c
-    :equilateral
-  elsif @a == @b || @a == @c || @b == @c
-    :isosceles
-  else :scalene
+    if @a  > 0 || @b > 0 || @c > 0
+      if @a == @b && @a == @c && @b == @c
+        :equilateral
+      elsif @a == @b || @a == @c || @b == @c
+        :isosceles
+      else :scalene
+      end
     end
   end
 
