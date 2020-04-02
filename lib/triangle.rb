cb00c0e8f44335681b30340b8d854b41d1b041ad
Yarvin Hernandez <yarhumher@hotmail.com>
2020-04-02 17:24:17 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,8 +14,9 @@ class Triangle
       else :scalene
       end
     else
-      if @a  == 0 || @b == 0 || @c == 0 || @a  < 0 || @b < 0 || @c < 0
+      # if @a  == 0 || @b == 0 || @c == 0
           raise TriangleError
+        # else
       end
 
     end
